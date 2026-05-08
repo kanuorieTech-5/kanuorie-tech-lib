34ac167a0a1d42715d845a7b47fa047bb0b0dd4e
Orie Kanu <kanuorie3@gmail.com>
2026-05-08 09:32:24 +0100

    modified:   frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
       
       <Routes>
         {/* PUBLIC ROUTES */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/settings" element={<Settings />} />
